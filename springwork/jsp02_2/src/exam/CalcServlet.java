583f36b5cf5aee5d7688e2c42440d85d1012c4e9
ghdwpaks <hjm7904@gmail.com>
2022-05-28 11:59:35 +0900

    jsp02 second class

---
+++ springwork/jsp02_2/src/exam/CalcServlet.java
@@ -0,0 +1,50 @@
+package exam;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CalcServlet extends HttpServlet{
+	
+	private Map<String, Calculator> map = new HashMap<>();
+	
+	public CalcServlet() {
+		map.put("/calc/add", new Add());
+		map.put("/calc/min", new Min());
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		process(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		process(req, resp);
+	}
+	private void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String uri = req.getRequestURI();
+		String command = uri.substring(req.getContextPath().length());
+		
+		String viewPage = "";
+		
+		if(command.equals("/calc")) {
+			viewPage = "/calcForm.jsp";
+		}else {
+			System.out.println("exam.CalcServlet.process.if.else.command :"+command);
+			//exam.CalcServlet.process.if.else.command :/calc/add
+			Calculator calc = map.get(command);
+			viewPage = calc.calc(req, resp);
+		}
+		
+		req.getRequestDispatcher(viewPage).forward(req, resp);
+	}
+}
+
+
+
+
---
+++ springwork/jsp02_2/src/exam/NowServlet.java
@@ -0,0 +1,33 @@
+package exam;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class NowServlet extends HttpServlet{
+	
+	public NowServlet() {
+		System.out.println("NowServlet");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Date now = new Date();
+		req.setAttribute("now", now);
+		req.getRequestDispatcher("/now.jsp").forward(req, resp);
+	}
+}
+
+
+
+
+
+
+
+
+
